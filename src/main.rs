52842a4b700f05009bf6284ba832037b79ef78e8
DillonChew98 <dillon_chew@a-star.edu.sg>
2026-07-10 02:30:35 +0800

    chore: change trace env default to info; add license

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+                .unwrap_or_else(|_| format!("{}=info", env!("CARGO_CRATE_NAME")).into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
